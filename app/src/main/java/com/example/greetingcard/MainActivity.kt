9350e6c93ff043ff4882395b572815515af4a5f3
Igor <30476065+Igor-san@users.noreply.github.com>
2023-11-07 20:38:52 +0300

    Practice: Compose article

--- app/src/main/java/com/example/greetingcard/MainActivity.kt
+++ app/src/main/java/com/example/greetingcard/MainActivity.kt
@@ -4,16 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
@@ -36,73 +32,97 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     //getString() is used for traditional Android development, while stringResource() is used in Jetpack Compose.
-                    GreetingImage(message = stringResource(R.string.happy_birthday_text), from = stringResource(
-                        R.string.signature_text
-                    ))
+//                    GreetingImage(message = stringResource(R.string.happy_birthday_text), from = stringResource(
+//                        R.string.signature_text
+//                    ))
+
+                    ComposeArticle()
                 }
             }
         }
     }
 }
 
-//@Composable
-//fun Greeting(name: String, modifier: Modifier = Modifier) {
-//    Surface(color = Color.Cyan) {
-//        Text(
-//            text = "Hi, my name is $name!",
-//            modifier = modifier.padding(24.dp)
-//        )
-//    }
-//}
-
+@Preview()
 @Composable
-fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
-    Column(verticalArrangement = Arrangement.Center, modifier = modifier) {
-        Text(
-            text = message,
-            fontSize = 80.sp,
-            lineHeight = 116.sp,
-            textAlign = TextAlign.Center
-        )
-        Text(
-            text = from,
-            fontSize = 36.sp,
-            modifier = Modifier
-                .padding(16.dp)
-                .align(alignment = Alignment.CenterHorizontally)
-        )
-    }
-}
-
-@Composable
-fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
-    val image = painterResource(R.drawable.androidparty)
-//Step 3 create a box to overlap image and texts
-    Box {
+fun ComposeArticle(modifier: Modifier = Modifier) {
+    val image = painterResource(R.drawable.bg_compose_background)
+    Column {
         Image(
             painter = image,
             contentDescription = null,
-            contentScale = ContentScale.Crop,
+            contentScale = ContentScale.FillWidth,
             alpha = 0.5F
         )
-
-        GreetingText(
-            message = message,
-            from = from,
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(8.dp)
+        Text(
+            text = stringResource(R.string.tutorial_title),
+            fontSize = 24.sp,
+            textAlign = TextAlign.Left,
+            modifier = modifier.padding(16.dp)
+        )
+        Text(
+            text = stringResource(R.string.tutorial_anons),
+            fontSize = 16.sp,
+            textAlign = TextAlign.Justify,
+            modifier = modifier.padding(16.dp)
+        )
+        Text(
+            text = stringResource(R.string.tutorial_text),
+            textAlign = TextAlign.Justify,
+            modifier = modifier.padding(16.dp)
         )
     }
 }
 
 
-@Preview(showBackground = true,
-    showSystemUi = true,
-    name="MyPreview")
-@Composable
-fun BirthdayCardPreview() {
-    HappyBirthdayTheme  {
-        GreetingImage("Happy Birthday Sam!", "From Emma")
-    }
-}
+//@Composable
+//fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+//    Column(verticalArrangement = Arrangement.Center, modifier = modifier) {
+//        Text(
+//            text = message,
+//            fontSize = 80.sp,
+//            lineHeight = 116.sp,
+//            textAlign = TextAlign.Center
+//        )
+//        Text(
+//            text = from,
+//            fontSize = 36.sp,
+//            modifier = Modifier
+//                .padding(16.dp)
+//                .align(alignment = Alignment.CenterHorizontally)
+//        )
+//    }
+//}
+//
+//@Composable
+//fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) {
+//    val image = painterResource(R.drawable.androidparty)
+////Step 3 create a box to overlap image and texts
+//    Box {
+//        Image(
+//            painter = image,
+//            contentDescription = null,
+//            contentScale = ContentScale.Crop,
+//            alpha = 0.5F
+//        )
+//
+//        GreetingText(
+//            message = message,
+//            from = from,
+//            modifier = Modifier
+//                .fillMaxSize()
+//                .padding(8.dp)
+//        )
+//    }
+//}
+//
+//
+//@Preview(showBackground = true,
+//    showSystemUi = true,
+//    name="MyPreview")
+//@Composable
+//fun BirthdayCardPreview() {
+//    HappyBirthdayTheme  {
+//        GreetingImage("Happy Birthday Sam!", "From Emma")
+//    }
+//}
